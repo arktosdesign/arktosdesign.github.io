f17cd11d4eefd35513fd584cba95412d4ffb8eb9
arktosdesign <44509036+arktosdesign@users.noreply.github.com>
2021-08-28 13:11:01 +0100

    Update falcon-madness.ecf9706e.js

--- falcon/falcon-madness.ecf9706e.js
+++ falcon/falcon-madness.ecf9706e.js
@@ -492,7 +492,7 @@ window.onload = function () {
   function tiltFalcon(e) {
     if (gameIsRunning) {
       var x = -e.accelerationIncludingGravity.x * 2.5;
-      var y = e.accelerationIncludingGravity.y;
+      var y = e.accelerationIncludingGravity.y - 4;
       var windowWidth = window.innerWidth;
       var windowHeight = window.innerHeight;
       var position = {
