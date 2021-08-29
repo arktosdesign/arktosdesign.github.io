587799c0ee236921d006c81a0b15a281f929d890
arktosdesign <44509036+arktosdesign@users.noreply.github.com>
2021-08-29 02:06:37 +0100

    PWA version

---
+++ falcon/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      return cache.addAll(
+        [
+          'index.html',
+          'styles.4201f784.css',
+          'falcon-madness.4ef24335.js',
+          'falcon-explode.mp3',
+          'laser.mp3',
+          'object-explode-1.mp3',
+          'object-explode-2.mp3'
+        ]
+      );
+    })
+  );
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWidth(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);  
+    })
+  );
+});
